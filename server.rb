a858c98401e00d7fb225e587a0e045f068932969
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-05-23 12:14:49 -0400

    working app

--- server.rb
+++ server.rb
@@ -16,15 +16,26 @@ get('/') do
   erb :index
 end
 
+get('/article/:title') do
+  title = params[:title]
+  article_data = read_article_data(ARTICLES)
+  @article = []
+  article_data.each do |a|
+    if a[:title] == title
+      @article << a
+    end
+    @article
+  end
+  erb :article
+end
 
 post('/posts') do
-
- title = params[:title]
- url = params[:url]
- description = params[:description]
+  title = params[:title]
+  url = params[:url]
+  description = params[:description]
 
   CSV.open(ARTICLES, 'a') do |file|
-    file << [title,url,description]
+  file << [title, url, description]
   end
   redirect '/'
 end

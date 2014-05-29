039508b6ef6e31db729a912885d7a1dfdd303707
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-05-29 17:16:29 -0400

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,26 @@
+-- CREATE TABLE ARTICLES (
+-- id SERIAL PRIMARY KEY,
+-- title varchar(200) NOT NULL,
+-- url text NOT NULL,
+-- description varchar(250),
+-- created_at timestamp NOT NULL
+-- );
+
+
+-- CREATE TABLE COMMENTS (
+-- id SERIAL PRIMARY KEY,
+-- article_id SERIAL,
+-- body text NOT NULL,
+-- created_at timestamp NOT NULL
+-- );
+
+
+-- *****************************************************
+-- actions
+-- INSERT INTO articles (title, url, description, created_at)
+-- VALUES ('', '', '', now());
+-- ******************************
+-- queries
+
+-- SELECT * FROM articles;
+-- SELECT * FROM comments;

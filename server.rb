40222f479807f53a080b11e63702056e148a6e76
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-05-30 11:00:32 -0400

    Core acceptance criteria compleate. Saving articles to slacker_news database.

--- server.rb
+++ server.rb
@@ -4,40 +4,40 @@ require 'pry'
 
 def db_connection
   begin
-    connection = PG.connect(dbname: 'movies')
+    connection = PG.connect(dbname: 'slacker_news')
     yield(connection)
   ensure
     connection.close
   end
 end
 
+def save_article(title, url, description)
+  sql = "INSERT INTO articles (title, url, description, created_at)" +
+    "VALUES ($1, $2, $3, NOW());"
 
+  connection = PG.connect(dbname: 'slacker_news')
+  connection.exec_params(sql, [title, url, description])
+  connection.close
+end
 
-get('/') do
-  @data_in = read_article_data(ARTICLES)
+get '/' do
+  query = 'SELECT articles.id, articles.title, articles.url, articles.description
+    FROM articles
+    ORDER BY created_at
+    LIMIT 20;'
+  @content = db_connection do |conn|
+    conn.exec(query)
+   end
   erb :index
 end
 
-get('/article/:title') do
-  title = params[:title]
-  article_data = read_article_data(ARTICLES)
-  @article = []
-  article_data.each do |a|
-    if a[:title] == title
-      @article << a
-    end
-    @article
-  end
-  erb :article
+get '/submit' do
+  erb :show
 end
 
-post('/posts') do
-  title = params[:title]
-  url = params[:url]
-  description = params[:description]
 
-  CSV.open(ARTICLES, 'a') do |file|
-  file << [title, url, description]
-  end
+post '/posts' do
+  save_article(params["title"], params["url"], params["description"])
+  # binding.pry
   redirect '/'
 end

b3b47d9a5e6b734e4d37821591bbe4e1c2674244
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-02 15:00:51 -0400

    Minor layout tweeks.

--- schema.sql
+++ schema.sql
@@ -1,29 +1,32 @@
--- CREATE TABLE ARTICLES (
--- id SERIAL PRIMARY KEY,
--- title varchar(200) NOT NULL,
--- url text NOT NULL,
--- description varchar(250),
--- created_at timestamp NOT NULL
--- );
+CREATE TABLE ARTICLES (
+id SERIAL PRIMARY KEY,
+title varchar(200) NOT NULL,
+url text NOT NULL,
+description varchar(250),
+created_at timestamp NOT NULL
+);
 
 
--- CREATE TABLE COMMENTS (
--- id SERIAL PRIMARY KEY,
--- article_id SERIAL,
--- body text NOT NULL,
--- created_at timestamp NOT NULL
--- );
+CREATE TABLE COMMENTS (
+id SERIAL PRIMARY KEY,
+article_id integer NOT NULL,
+body text NOT NULL,
+created_at timestamp NOT NULL
+);
 
 
--- *****************************************************
--- actions
--- INSERT INTO articles (title, url, description, created_at)
--- VALUES ('', '', '', now());
--- ******************************
--- queries
+*****************************************************
+actions
+INSERT INTO articles (title, url, description, created_at)
+VALUES ('', '', '', now());
+******************************
+queries
 
--- SELECT * FROM articles;
--- SELECT * FROM comments;
--- DELETE FROM articles WHERE ________;
--- UPDATE table_name SET column_name = new_value WHERE conditional_filter;
--- UPDATE articles SET title = This is way cool! WHERE id = 1;
+SELECT * FROM articles;
+SELECT * FROM comments;
+DELETE FROM articles WHERE ________;
+UPDATE table_name SET column_name = new_value WHERE conditional_filter;
+UPDATE articles SET title = This is way cool! WHERE id = 1;
+
+
+DELETE FROM comments WHERE body = 'This article sucks!';
---
+++ test.rb
@@ -0,0 +1,27 @@
+space_quora
+def save_question(content)
+  sql = "INSERT INTO questions (content, created_at) " +
+    "VALUES ($1, NOW())"
+
+  connection = PG.connect(dbname: 'space_quora')
+  connection.exec_params(sql, [content])
+  connection.close
+end
+
+# def find_questions
+  connection = PG.connect(dbname: 'space_quora')
+  results = connection.exec('SELECT * FROM questions')
+  connection.close
+
+  results
+end
+
+get '/questions' do
+  @questions = find_questions
+  erb :'questions/index.html'
+end
+
+post '/questions' do
+  save_question(params["content"])
+  redirect '/questions'
+end

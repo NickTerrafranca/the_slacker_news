ebacfc8dd8b25188c0b385e414183ed886881451
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-02 18:20:19 -0400

    Added a querie for display comment method

--- schema.sql
+++ schema.sql
@@ -14,6 +14,27 @@ body text NOT NULL,
 created_at timestamp NOT NULL
 );
 
+SELECT articles.id, articles.title, articles.url, articles.description
+    FROM articles
+    ORDER BY created_at
+    LIMIT 20;
+
+SELECT articles.id, articles.title, articles.url, articles.description
+  FROM articles
+  WHERE articles.id = $1;
+
+SELECT article_id, body, created_at
+    FROM comments
+    WHERE article_id = 1;
+
+
+  "INSERT INTO articles (title, url, description, created_at)" +
+    "VALUES ($1, $2, $3, NOW());"
+
+    "INSERT INTO comments (article_id, body, created_at)" +
+    "VALUES ($1, $2, NOW());"
+
+
 
 *****************************************************
 actions
@@ -29,4 +50,3 @@ UPDATE table_name SET column_name = new_value WHERE conditional_filter;
 UPDATE articles SET title = This is way cool! WHERE id = 1;
 
 
-DELETE FROM comments WHERE body = 'This article sucks!';
